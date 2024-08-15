10fc9ba4a40c493b833c8c89b2b917f899f2eb5e
miceflicts <nickzinhomice@gmail.com>
2024-08-15 17:06:49 -0300

    Feat: Auto call api every 14 minutes

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import route from "./routes/userRoute.js";
+import axios from "axios";
 
 const app = express();
 app.use(express.json());
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 const MONGOURL = process.env.MONGO_URL;
+const url = "https://tribofy-api.onrender.com/api/user/getAllUsers";
+const interval = 840000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 mongoose
   .connect(MONGOURL)
