3acda2003c69ac8e6536f204c71a5354978b7049
miceflicts <nickzinhomice@gmail.com>
2024-08-18 17:10:12 -0300

    Feat: New CORS policy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import axios from "axios";
+import cors from "cors";
 
 import userRoute from "./routes/usersRoute.js";
 import communityRoute from "./routes/communitiesRoute.js";
@@ -16,6 +17,27 @@ const PORT = process.env.PORT || 4000;
 const MONGOURL = process.env.MONGO_URL;
 const url = "https://tribofy-api.onrender.com/api/user/getAllUsers";
 const interval = 840000;
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",")
+  : ["http://localhost:3000"]; // Fallback para desenvolvimento local
+
+// Configuração do CORS
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 function reloadWebsite() {
   axios
