4658bf59e242cac0b061a1a277119635c133520d
miceflicts <nickzinhomice@gmail.com>
2024-08-24 20:52:42 -0300

    Feat: Changed login method from put to post

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -13,7 +13,7 @@ const userRoute = express.Router();
 userRoute.post("/create", create);
 userRoute.get("/getAllUsers", fetch);
 userRoute.put("/update/:id", update);
-userRoute.put("/login", login);
+userRoute.post("/login", login);
 userRoute.delete("/delete/:id", deleteUser);
 
 export default userRoute;
